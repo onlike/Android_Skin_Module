74a7b3e87b7e8731dc5f0237ec4057cc26382b6d
onlike <lvxueyan_work@163.com>
2018-03-12 19:26:59 +0800

    add test code in MainActivity

---
+++ app/src/main/java/com/lxy/demo_applicaionskin/adapter/MainAdapter.java
@@ -0,0 +1,82 @@
+package com.lxy.demo_applicaionskin.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lxy.demo_applicaionskin.R;
+import com.lxy.demo_applicaionskin.bean.TestABean;
+import com.lxy.demo_applicaionskin.listener.IDataBinding;
+import com.lxy.module.skin.util.LSkinUtils;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lxy on 2018/3/12.
+ * 
+ */
+public class MainAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    
+    private Context mContext ;
+    
+    private ArrayList<TestABean> mData;
+
+    public MainAdapter(Context context) {
+        this.mContext   = context;
+        this.mData      = new ArrayList<>();
+    }
+    
+    public void addData(ArrayList<TestABean> data){
+        if (LSkinUtils.array_isEmpty(data)) return;
+        
+        mData.addAll(data);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(mContext).inflate(R.layout.item_rv_main, parent, false);
+        return new Holder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((IDataBinding)holder).bindData(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+    
+    private class Holder 
+                extends 
+                    RecyclerView.ViewHolder 
+                implements 
+                    IDataBinding
+    {
+
+        private TextView tvTitle;
+        
+        private TextView tvSubTitle;
+        
+        Holder(View itemView) {
+            super(itemView);
+            
+            tvTitle     = itemView.findViewById(R.id.tv_item_main_title);
+            tvSubTitle  = itemView.findViewById(R.id.tv_item_main_sub_title);
+        }
+
+        @Override
+        public void bindData(int position) {
+            TestABean bean = mData.get(position);
+            
+            tvTitle.setText(bean.title);
+            tvSubTitle.setText(bean.subTitle);
+            
+        }
+    }
+}
--- app/src/main/java/com/lxy/demo_applicaionskin/application/LApplication.java
+++ app/src/main/java/com/lxy/demo_applicaionskin/application/LApplication.java
@@ -1,4 +1,4 @@
-package com.lxy.demo_applicaionskin;
+package com.lxy.demo_applicaionskin.application;
 
 import android.app.Application;
 import android.util.Log;
@@ -6,13 +6,15 @@ import android.util.Log;
 import com.lxy.module.skin.SkinManager;
 
 /**
- * Created by linksus on 2018/3/1.
+ * Created by lxy on 2018/3/1.
+ * 
  */
-
 public class LApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        
+        // you must init module at application run first time
         SkinManager.init(this);
 
         Log.e("onlike","---------------------------------------"+this.getPackageName());
